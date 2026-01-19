38215a34222540e927067727faa37c81bb54759c
Anantha Krishnan <ananthak803@gmail.com>
2026-01-19 15:16:01 +0530

    allow all origin

--- backend/main.go
+++ backend/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"time"
 
 	"shrink/routes"
 
@@ -57,20 +56,13 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins: []string{
-			"https://shrinkurl-lopp.onrender.com/",
-		},
+		AllowAllOrigins: true,
 		AllowMethods: []string{
 			"GET", "POST", "PUT", "DELETE", "OPTIONS",
 		},
 		AllowHeaders: []string{
 			"Origin", "Content-Type", "Authorization",
 		},
-		ExposeHeaders: []string{
-			"Content-Length",
-		},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
 	}))
 
 	// Health check endpoint
